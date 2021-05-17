59f77594cb8eb0edf75a36f998f95795c8fee31e
lizadlgv <liza.dolgova2002@mail.ru>
2021-05-17 22:04:20 +0400

    Обмен данными в формате XML

--- src/main/java/ru/ssau/tk/lizadlgv/tp_lab/Controller.java
+++ src/main/java/ru/ssau/tk/lizadlgv/tp_lab/Controller.java
@@ -1,14 +1,14 @@
 package ru.ssau.tk.lizadlgv.tp_lab;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.ssau.tk.lizadlgv.tp_lab.game.Game;
 import ru.ssau.tk.lizadlgv.tp_lab.game.GameException;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
     private final Game game;
 
@@ -18,58 +18,58 @@ public class Controller {
     }
 
     @ResponseBody
-    @GetMapping(value = "/connect", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/connect")
     public Answer connect() {
         try {
             return Answer.ok(game.addNewPlayer());
         } catch (GameException e) {
             return Answer.error(e.getMessage());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return Answer.error("Произошла ошибка на сервере");
         }
     }
 
     @ResponseBody
-    @GetMapping(value = "/go", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/go")
     public Answer go(@RequestParam double angle, @RequestParam String token) {
         try {
             return Answer.ok(game.step(token, angle));
         } catch (GameException e) {
             return Answer.error(e.getMessage());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return Answer.error("Произошла ошибка на сервере");
         }
     }
 
     @ResponseBody
-    @GetMapping(value = "/canGo", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/canGo")
     public Answer canGo(@RequestParam String token) {
         try {
             return Answer.ok(game.canStep(token));
         } catch (GameException e) {
             return Answer.error(e.getMessage());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return Answer.error("Произошла ошибка на сервере");
         }
     }
 
     @ResponseBody
-    @GetMapping(value = "/validateToken", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/validateToken")
     public Answer validateToken(@RequestParam String token) {
         return Answer.ok(game.validateToken(token));
     }
 
     @ResponseBody
-    @GetMapping(value = "/getState", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/getState")
     public Answer getSate(@RequestParam String token) {
         try {
             return Answer.ok(game.getState(token));
         } catch (GameException e) {
             return Answer.error(e.getMessage());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return Answer.error("Произошла ошибка на сервере");
         }
